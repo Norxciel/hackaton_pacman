2729f499face2d9eca388f724c92ce0e71ef7f48
Florian Morel <morel.florian80@hotmail.fr>
2021-05-17 14:01:46 +0200

    add dossier assets + dossier Sound + function Sound dans le fichier app.js

---
+++ public/assets/js/music.js
@@ -0,0 +1,5 @@
+let startGame = new Audio('assets/sound/pacman_beginning.wav');
+let eatGhost = new Audio('assets/sound/pacman_eatghost.wav');
+let pacManMove = new Audio('assets/sound/pacman_chomp.wav');
+let death = new Audio('assets/sound/pacman_death.wav');
+let intermission = new Audio('assets/sound/pacman_intermission.wav');
